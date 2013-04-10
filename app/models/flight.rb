d0ca6127e847531b6aa2b9a8d587371861148709
Apprentice <apprentice@devbootcamp.com>
2013-04-10 00:17:36 -0700

    3.3s monster query

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -17,6 +17,16 @@ class Flight < ActiveRecord::Base
     Flight.where(flight_no: flight_no, departure_time: departure_time,
       departure_airport_id: departure_airport_id, arrival_airport_id: arrival_airport_id)
   end
+  
+  def self.similar_flights
+    select('flights.id AS original_flight_id, similar_flights.*').
+    joins('INNER JOIN flights AS similar_flights ON
+      flights.flight_no             = similar_flights.flight_no AND
+      flights.departure_time        = similar_flights.departure_time AND
+      flights.departure_airport_id  = similar_flights.departure_airport_id').
+    where('flights.is_first_flight = \'t\' AND flights.number_of_stops > 0').
+    order('original_flight_id asc, similar_flights.price asc').all.group_by { |f| f.original_flight_id }
+  end
 
   def self.non_stops
     where(number_of_stops: 0)
@@ -28,6 +38,42 @@ class Flight < ActiveRecord::Base
 
   def rounded_price
     price / 100 + 1
+  end
+
+  def self.with_connections
+    where('number_of_stops > 0')
+  end
+
+  def self.first_flights
+    where(is_first_flight: true)
+  end
+
+  def self.get_shortcuts
+    shortcuts = []
+    similar_flights.each do |original_flight_id, similar_flights|
+      add_shortcuts(shortcuts, similar_flights)
+    end
+    shortcuts
+  end
+
+  def self.add_shortcuts(shortcuts, similar_flights)
+
+    cheapest_flight = similar_flights.first
+    return if cheapest_flight.non_stop?
+    non_stop_flight = similar_flights.find {|f| f.number_of_stops == 0 }
+
+    if non_stop_flight
+      shortcuts << [cheapest_flight, non_stop_flight]    
+    end
+
+    # this array is the first flight of its itinerary
+    # it may be on its own or with another flight
+    # find the cheapest one in the group
+    # if its non-stop then no shortcut (return)
+    # keep the shortcut flight and the nonstop in the group
+
+    # level 3
+
   end
 
 end
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -11,42 +11,4 @@ class Itinerary < ActiveRecord::Base
 							:class_name => Airport,
 							:foreign_key => 'destination_airport_id'
 
-  def first_flight
-    flights.order(:departure_time).first
-  end
-
-  class << self
-
-    def with_connections
-      where('flights_count > 1')
-    end
-
-    def get_shortcuts
-      shortcuts = []
-      with_connections.each do |itinerary|
-        itinerary.add_shortcuts(shortcuts)
-      end
-      shortcuts
-    end
-
-  end
-
-  def add_shortcuts(shortcuts)
-    similar_flights = self.first_flight.similar_flights
-
-    # this array is the first flight of its itinerary
-    # it may be on its own or with another flight
-    # find the cheapest one in the group
-    # if its non-stop then no shortcut (return)
-    # keep the shortcut flight and the nonstop in the group
-
-    # level 3
-    cheapest_flight = similar_flights.order('price asc').first
-    return if cheapest_flight.non_stop?
-    non_stop_flight = similar_flights.non_stops.first
-
-    if non_stop_flight
-      shortcuts << [cheapest_flight, non_stop_flight]    
-    end
-  end
 end
