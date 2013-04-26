2c8da9293e9d1057e0ff36ef7e0f6421056c5ab1
Dawenster <dstwen@gmail.com>
2013-04-26 12:44:13 -0700

    removed comments from offsite links controller; added opening of links in new tab

--- app/controllers/offsite_flights_controller.rb
+++ app/controllers/offsite_flights_controller.rb
@@ -9,22 +9,13 @@ class OffsiteFlightsController < ApplicationController
       arrival_airport = original_flight.arrival_airport.code
       itin_id = original_flight.uid[0..1].gsub("-", "").gsub("_", "")
 
-
       link = "http://travel.travelocity.com/flights/CrossSellCheckoutHandOff.do;jsessionid=E1F995747D3C4538FC8DA80BF226B3DF.pwbap033a?flightPath=TF&tripType=oneway#{(Time.now + 2.hours).to_s[0..18].gsub(/\D/,'')}&flightType=oneway&dateTypeSelect=exactDates&adults=1&children=0&minorsAge0=%3F&minorsAge1=%3F&minorsAge2=%3F&minorsAge3=%3F&minorsAge4=%3F&seniors=0&classOfService=ECONOMY&fareType=all&membershipLevel=NO_VALUE&airlineSearchPref=&leavingFrom=#{departure_airport.code}&goingTo=#{arrival_airport}&leavingDate=#{flight.departure_time.strftime('%m %d %Y').gsub(' ', '%2F')}&dateLeavingTime=Anytime&originalLeavingTime=Anytime&lastSelectedLeg=#{original_flight.uid}&selectedLeg0=&selectedLeg1=&selectedLeg2=&itinId=#{itin_id}&rid=#{original_flight.rid}&originalPrice=&originalTotalPrice="
-
-      # link = "https://travel.travelocity.com/checkout/CheckoutReview.do?flightPath=TF&tripType=oneway#{(Time.now + 2.hours).to_s[0..18].gsub(/\D/,'')}&flightType=oneway&dateTypeSelect=exactDates&adults=1&children=0&minorsAge0=%3F&minorsAge1=%3F&minorsAge2=%3F&minorsAge3=%3F&minorsAge4=%3F&seniors=0&classOfService=ECONOMY&fareType=all&membershipLevel=NO_VALUE&airlineSearchPref=&leavingFrom=#{departure_airport.code}&goingTo=#{arrival_airport}&leavingDate=#{flight.departure_time.strftime('%m %d %Y').gsub(' ', '%2F')}&dateLeavingTime=Anytime&originalLeavingTime=Anytime&lastSelectedLeg=#{original_flight.uid}&selectedLeg0=&selectedLeg1=&selectedLeg2=&itinId=#{itin_id}&rid=#{original_flight.rid}&originalPrice=&originalTotalPrice="
-
     else
       itinerary_destination = flight.itinerary.destination_airport
       itin_id = flight.uid[0..1].gsub("-", "").gsub("_", "")
 
       link = "http://travel.travelocity.com/flights/CrossSellCheckoutHandOff.do;jsessionid=E1F995747D3C4538FC8DA80BF226B3DF.pwbap033a?flightPath=TF&tripType=oneway#{(Time.now + 2.hours).to_s[0..18].gsub(/\D/,'')}&flightType=oneway&dateTypeSelect=exactDates&adults=1&children=0&minorsAge0=%3F&minorsAge1=%3F&minorsAge2=%3F&minorsAge3=%3F&minorsAge4=%3F&seniors=0&classOfService=ECONOMY&fareType=all&membershipLevel=NO_VALUE&airlineSearchPref=&leavingFrom=#{departure_airport.code}&goingTo=#{itinerary_destination.code}&leavingDate=#{flight.departure_time.strftime('%m %d %Y').gsub(' ', '%2F')}&dateLeavingTime=Anytime&originalLeavingTime=Anytime&lastSelectedLeg=#{flight.uid}&selectedLeg0=&selectedLeg1=&selectedLeg2=&itinId=#{itin_id}&rid=#{flight.rid}&originalPrice=&originalTotalPrice="
-
-      # link = "https://travel.travelocity.com/checkout/CheckoutReview.do?flightPath=TF&tripType=oneway#{(Time.now + 2.hours).to_s[0..18].gsub(/\D/,'')}&flightType=oneway&dateTypeSelect=exactDates&adults=1&children=0&minorsAge0=%3F&minorsAge1=%3F&minorsAge2=%3F&minorsAge3=%3F&minorsAge4=%3F&seniors=0&classOfService=ECONOMY&fareType=all&membershipLevel=NO_VALUE&airlineSearchPref=&leavingFrom=#{departure_airport.code}&goingTo=#{itinerary_destination.code}&leavingDate=#{flight.departure_time.strftime('%m %d %Y').gsub(' ', '%2F')}&dateLeavingTime=Anytime&originalLeavingTime=Anytime&lastSelectedLeg=#{flight.uid}&selectedLeg0=&selectedLeg1=&selectedLeg2=&itinId=#{itin_id}&rid=#{flight.rid}&originalPrice=&originalTotalPrice="
     end
-    # binding.pry
-
     redirect_to link
-
   end
 end
