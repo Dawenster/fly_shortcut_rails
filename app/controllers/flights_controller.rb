62bfc2063dfe95eb3c3cbe4c1209c69dd7f36101
Dawenster <dstwen@gmail.com>
2013-04-28 16:55:44 -0700

    removed duplicate flights - picked the cheaper one

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,6 +5,9 @@ class FlightsController < ApplicationController
     @to = []
     @dates = []
 
+    @flights.sort_by! { |flight| flight[0].price }
+    @flights.uniq! { |flight| flight[0].flight_no + flight[0].airline }
+
     @flights.each do |flight|
       @from << flight[0].departure_airport.name
       @to << flight[0].arrival_airport.name
