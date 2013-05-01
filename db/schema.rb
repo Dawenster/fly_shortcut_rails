f4fe15452b7e5a38f24288af8b502873a9d7d055
Adrian Khoo <adriankhoo.ca@gmail.com>
2013-05-01 11:59:55 -0700

    Removed binding.pry

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130409230702) do
+ActiveRecord::Schema.define(:version => 20130430065736) do
 
   create_table "airports", :force => true do |t|
     t.string   "code"
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(:version => 20130409230702) do
     t.integer  "price"
     t.integer  "number_of_stops"
     t.boolean  "is_first_flight"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.integer  "second_flight_destination"
     t.integer  "second_flight_no"
     t.integer  "original_price"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
   end
 
   add_index "flights", ["arrival_airport_id"], :name => "index_flights_on_arrival_airport_id"
