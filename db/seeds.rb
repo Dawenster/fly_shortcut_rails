36c8fde53ec7256275aba16bd8be73b85536bd0a
Dawenster <dstwen@gmail.com>
2013-05-02 18:55:11 -0700

    removed secondary hit to scrape one-stop flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,105 +27,90 @@ Dir[Rails.root.join('db/routes/*.csv')].each do |file|
   puts "Scraping flights originating from #{origin_code}"
 
   date_array.each do |date|
-    Flight.where(:origin_code => origin_code, :pure_date => date).destroy_all
+    Flight.where(:origin_code => origin_code).destroy_all
 
     CSV.foreach(file) do |route|
-      origin = route[0]
-      origin_airport_id = Airport.find_by_code(origin).id
-
-      destination = route[1]
-      destination_airport_id = Airport.find_by_code(destination).id
-
-      formatted_date = date.split("/").rotate(2).join("-")
-
-      puts "*" * 50
-      puts "#{origin}-#{destination}-#{date}"
-      puts "*" * 50
-
       begin
+        origin = route[0]
+        origin_airport_id = Airport.find_by_code(origin).id
+
+        destination = route[1]
+        destination_airport_id = Airport.find_by_code(destination).id
+
+        formatted_date = date.split("/").rotate(2).join("-")
+
+        puts "*" * 50
+        puts "#{origin}-#{destination}-#{date}"
+        puts "*" * 50
+
         search_result = JSON.parse(RestClient.get 'http://travel.travelocity.com/flights/FlightsItineraryService.do', params: { jsessionid: 'ACEE3FCA20509BA3931D4E79C822E310.pwbap099a', flightType: 'oneway', dateTypeSelect: 'EXACT_DATES', leavingDate: date, leavingFrom: origin, goingTo: destination, dateLeavingTime: 1200, originalLeavingTime: 'Anytime', adults: 1, seniors: 0, children: 0, paxCount: 1, classOfService: 'ECONOMY', fareType: 'all', membershipLevel: 'NO_VALUE' })
 
-        rid = search_result["metadata"]["responseId"]
         itins = search_result["results"]
-
         itins.each do |itin|
-          if itin["numberOfStops"] == 0
-            created_flight = Flight.create! do |fl|
-              fl.departure_airport_id = origin_airport_id
-              fl.arrival_airport_id = destination_airport_id
-              fl.departure_time = DateTime.strptime(formatted_date + '-' + itin["departureTimeDisplay"], '%Y-%m-%d-%I:%M %p')
-              fl.arrival_time = DateTime.strptime(formatted_date + '-' + itin["arrivalTimeDisplay"], '%Y-%m-%d-%I:%M %p')
-              fl.arrival_time = fl.arrival_time + 1.day if fl.arrival_time < fl.departure_time
-              fl.airline = itin["airline"]
-              fl.flight_no = itin["header"][0]["flightNumber"]
-              fl.price = (itin['totalFare'].to_f * 100).to_i
-              fl.number_of_stops = 0
-              fl.is_first_flight = true
-              fl.pure_date = date
-            end
-
-            puts "Scraped Non-stop #{itin["airline"]} #{itin["header"][0]["flightNumber"]}"
-            flight_count += 1
-          end
-
-          if itin["numberOfStops"] == 1
-            uid = itin["uniqueId"]
-
-            begin
-              itinerary = JSON.parse(RestClient.get 'http://travel.travelocity.com/flights/FlightsDetails.do', params: { jsessionid: '0', locLink: 'OUTBOUND|NAT1', ashopRid: rid, itins: uid, classOfService: 'ECONOMY', paxCount: 1, leavingFrom: origin, goingTo: destination })
-
-              if itinerary['details'].length == 2
-                first_flight = itinerary['details'][0]
-                second_flight = itinerary['details'][1]
-                next_day = false
-
-                flight1 = Flight.create! do |fl|
-                  fl.departure_airport_id = origin_airport_id
-                  fl.arrival_airport_id = Airport.find_by_code(first_flight['details-arrivalLocation'][/\(...\)/].gsub(/\W/, '')).id
-                  fl.departure_time = DateTime.strptime(formatted_date + '-' + first_flight['details-departureTime'], '%Y-%m-%d-%I:%M %p')
-                  fl.arrival_time = DateTime.strptime(formatted_date + '-' + first_flight['details-arrivalTime'], '%Y-%m-%d-%I:%M %p')
-                  if fl.arrival_time < fl.departure_time
-                    fl.arrival_time += 1.day
-                    next_day = true
-                  end
-                  fl.airline = first_flight['details-airline']
-                  fl.flight_no = first_flight['details-flightNumber']
-                  fl.price = (first_flight['details-totalFare'].to_f * 100).to_i
-                  fl.number_of_stops = 1
-                  fl.is_first_flight = true
-                  fl.pure_date = date
-                end
-
-                flight2 = Flight.create! do |fl|
-                  fl.departure_airport_id = Airport.find_by_code(second_flight['details-departureLocation'][/\(...\)/].gsub(/\W/, '')).id
-                  fl.arrival_airport_id = destination_airport_id
-                  fl.departure_time = DateTime.strptime(formatted_date + '-' + second_flight['details-departureTime'], '%Y-%m-%d-%I:%M %p')
-                  fl.arrival_time = DateTime.strptime(formatted_date + '-' + second_flight['details-arrivalTime'], '%Y-%m-%d-%I:%M %p')
-                  if next_day || fl.departure_time < flight1.arrival_time
-                    fl.departure_time += 1.day
-                    fl.arrival_time += 1.day
-                  end
-                  fl.arrival_time += 1.day if fl.arrival_time < fl.departure_time
-                  fl.airline = second_flight['details-airline']
-                  fl.flight_no = second_flight['details-flightNumber']
-                  fl.price = (second_flight['details-totalFare'].to_f * 100).to_i
-                  fl.number_of_stops = 1
-                  fl.is_first_flight = false
-                  fl.pure_date = date
-                end
-                
-                flight1.update_attributes(:second_flight_destination => flight2.arrival_airport_id, :second_flight_no => flight2.flight_no)
-
-                puts "Scraped One-stop #{first_flight["details-airline"]} #{first_flight['details-flightNumber']} and #{second_flight['details-flightNumber']}"
-                flight_count += 2
+          begin
+            if itin["numberOfStops"] == 0
+              created_flight = Flight.create! do |fl|
+                fl.departure_airport_id = origin_airport_id
+                fl.arrival_airport_id = destination_airport_id
+                fl.departure_time = DateTime.strptime(formatted_date + '-' + itin["departureTimeDisplay"], '%Y-%m-%d-%I:%M %p')
+                fl.arrival_time = DateTime.strptime(formatted_date + '-' + itin["arrivalTimeDisplay"], '%Y-%m-%d-%I:%M %p')
+                fl.arrival_time = fl.arrival_time + 1.day if fl.arrival_time < fl.departure_time
+                fl.airline = itin["airline"]
+                fl.flight_no = itin["header"][0]["flightNumber"]
+                fl.price = (itin['totalFare'].to_f * 100).to_i
+                fl.number_of_stops = 0
+                fl.is_first_flight = true
+                fl.pure_date = date
               end
-            rescue
+
+              puts "Scraped Non-stop #{itin["airline"]} #{itin["header"][0]["flightNumber"]}"
+              flight_count += 1
             end
+
+            if itin["numberOfStops"] == 1
+              next_day = false
+
+              flight1 = Flight.create! do |fl|
+                fl.departure_airport_id = origin_airport_id
+                fl.departure_time = DateTime.strptime(formatted_date + '-' + itin['departureTimeDisplay'], '%Y-%m-%d-%I:%M %p')
+                fl.airline = itin['header'][0]['airline']
+                fl.flight_no = itin['header'][0]['flightNumber']
+                fl.price = (itin['totalFare'].to_f * 100).to_i
+                fl.number_of_stops = 1
+                fl.is_first_flight = true
+                fl.pure_date = date
+              end
+
+              flight2 = Flight.create! do |fl|
+                fl.arrival_airport_id = destination_airport_id
+                fl.airline = itin['header'][1]['airline']
+                fl.flight_no = itin['header'][1]['flightNumber']
+                fl.price = (itin['totalFare'].to_f * 100).to_i
+                fl.number_of_stops = 1
+                fl.is_first_flight = false
+                fl.pure_date = date
+              end
+              
+              flight1.update_attributes(:second_flight_destination => flight2.arrival_airport_id, :second_flight_no => flight2.flight_no)
+
+              puts "Scraped One-stop #{itin['header'][0]['airline']} #{itin['header'][0]['flightNumber']} and #{itin['header'][1]['flightNumber']}"
+              flight_count += 2
+            end
+          rescue
           end
         end
       rescue
       end
     end
+    puts "*" * 50
+    puts "Filling in one-stop flight info..."
+
+    incomplete_flights = Flight.where(:is_first_flight => true, :number_of_stops => 1)
+    incomplete_flights.each do |flight|
+      match = Flight.where(:flight_no => flight.flight_no, :pure_date => flight.pure_date, :number_of_stops => 0)[0]
+      flight.update_attributes(:arrival_airport_id => match.arrival_airport_id, :arrival_time => match.arrival_time) if match
+    end
+
     puts "*" * 50
     puts "Commencing shortcut calculations..."
 
