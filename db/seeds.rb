1632821740ef7326905454d0e1dd7d544f47cf86
Dawenster <dstwen@gmail.com>
2013-05-08 19:29:57 -0700

    changed num_days in seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ flight_count = 0
 Dir[Rails.root.join('db/routes/*.csv')].each do |file|
   origin_code = file.split('/').last[0..2]
   date_array = []
-  num_days = [1,31,62] #(1..2).to_a
+  num_days = (1..30).to_a
 
   num_days.each do |num|
     date_array << (Time.now + num.days).strftime('%m/%d/%Y')
