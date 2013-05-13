b11600b8b59f78604a684b5748f1ab420e9f39b9
Dawenster <dstwen@gmail.com>
2013-05-12 18:23:01 -0700

    fixed twitter problem

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -273,7 +273,6 @@ $(document).ready(function() {
         $('#from-dropdown').removeAttr('disabled').removeClass('disabled');
         $('#to-dropdown').removeAttr('disabled').removeClass('disabled');
         updateActive(thisButton);
-        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
         $('#second-email-button').click(function(e) {
           e.preventDefault();
 
