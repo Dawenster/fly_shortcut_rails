51005334f4e89840e7816e1f7cb450ba16f634d4
Dawenster <dstwen@gmail.com>
2013-05-20 18:46:58 -0700

    fixed second button loading problem on index

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('#let-me-proceed').click(function(){
     $('#let-me-proceed').button('loading');
   });
+
+  $('#let-me-proceed2').click(function(){
+    $('#let-me-proceed2').button('loading');
+  });
 });
