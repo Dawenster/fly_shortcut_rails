2d9a04a3c033d61d4d6e03e2369fd62b9675558e
Dawenster <dstwen@gmail.com>
2013-07-22 15:04:31 -0700

    updated events code for book button

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
-	$('#play-test').on('click', function(e) {
-		e.preventDefault();
-	  _gaq.push(['_trackEvent', 'Test', 'Click', 'Testing GA']);
-	});
-});
+// $(document).ready(function() {
+// 	$('#play-test').on('click', function(e) {
+// 		e.preventDefault();
+// 	  _gaq.push(['_trackEvent', 'Test', 'Click', 'Testing GA']);
+// 	  console.log("should have sent tracker");
+// 	});
+// });
