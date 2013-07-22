3c0e7a4c660510fbabba431979d40b35159c2df1
Dawenster <dstwen@gmail.com>
2013-07-22 13:32:59 -0700

    preventing default on the link on ga_test page

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,5 +1,6 @@
-// $(document).ready(function() {
-// 	$('.book-button').on('click', function() {
-// 	  ga('send', 'event', 'button', 'click', 'book-button');
-// 	});
-// });
+$(document).ready(function() {
+	$('#play-test').on('click', function(e) {
+		e.preventDefault();
+	  _gaq.push(['_trackEvent', 'Test', 'Click', 'Testing GA']);
+	});
+});
