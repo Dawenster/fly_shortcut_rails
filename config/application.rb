77adff5ce9880db07d3cc9caaae9879a40a30fe1
Dawenster <dstwen@gmail.com>
2013-12-13 20:03:55 -0800

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module FlyShortcutRails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # For geocoder
+    config.assets.initialize_on_precompile = false
   end
 end
