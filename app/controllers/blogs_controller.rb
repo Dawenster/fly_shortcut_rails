0c39a46e6e60332931064d6d7fad6134afa65256
David Wen <dstwen@gmail.com>
2014-02-05 18:07:59 +0800

    change live blog to flyshortcut

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   def index
     page = params[:page].to_i || 0
     client = Tumblr::Client.new
-    tumblr_posts = client.posts("dawenster.tumblr.com", :filter => "text", :offset => page * 20)
+    tumblr_posts = client.posts("flyshortcut.tumblr.com", :filter => "text", :offset => page * 20)
     @posts = tumblr_posts["posts"]
     @user = User.new
     @airports = Airport.all.map { |airport| airport.name }.unshift("(Desired departure airport)")
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
 
   def show
     client = Tumblr::Client.new
-    @post = client.posts("dawenster.tumblr.com", :tag => params[:slug])["posts"][0]
+    @post = client.posts("flyshortcut.tumblr.com", :tag => params[:slug])["posts"][0]
     @user = User.new
     @airports = Airport.all.map { |airport| airport.name }.unshift("(Desired departure airport)")
   end
