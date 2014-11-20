1996f473040cd296b624745ff2a139c65851b0bf
David Wen <dstwen@gmail.com>
2014-11-20 12:31:39 -0800

    only show alert on index page

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
-  setTimeout(function() {
-    swal({
-      title: "We're taking a break :)",
-      text: "Sorry for the inconvenience...",
-      type: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#DD6B55",
-      confirmButtonText: "Sign up for alerts",
-      cancelButtonText: "How come?!",
-      closeOnConfirm: false,
-      closeOnCancel: false,
-      allowOutsideClick: true
-    }, 
-    function(isConfirm){
-      var url = ""
-      if (isConfirm) {
-        url = "/blog";
-      } else {
-        url = "http://www.bloomberg.com/news/2014-11-18/united-orbitz-sue-travel-site-over-hidden-city-ticketing-1-.html";
-      }
-      window.location = url;
-    });
-  }, 500);
+  if ($(".panel1Container").length > 0) {
+    setTimeout(function() {
+      swal({
+        title: "We're taking a break :)",
+        text: "Sorry for the inconvenience...",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Sign up for alerts",
+        cancelButtonText: "How come?!",
+        closeOnConfirm: false,
+        closeOnCancel: false,
+        allowOutsideClick: true
+      }, 
+      function(isConfirm){
+        var url = ""
+        if (isConfirm) {
+          url = "/blog";
+        } else {
+          url = "http://www.bloomberg.com/news/2014-11-18/united-orbitz-sue-travel-site-over-hidden-city-ticketing-1-.html";
+        }
+        window.location = url;
+      });
+    }, 500);
+  }
 
   $('.let-me-proceed').click(function(e){
     e.preventDefault();
