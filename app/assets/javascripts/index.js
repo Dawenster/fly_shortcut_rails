b443f279fbc9c106647035e192e0d20e77a8ea65
David Wen <dstwen@gmail.com>
2014-11-20 11:28:27 -0800

    add popup immediately when users land on /

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+  setTimeout(function() {
+    swal({
+      title: "We're taking a break :)",
+      text: "Sorry for the inconvenience...",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Sign up for alerts",
+      cancelButtonText: "How come?!",
+      closeOnConfirm: false,
+      closeOnCancel: false,
+      allowOutsideClick: true
+    }, 
+    function(isConfirm){
+      var url = ""
+      if (isConfirm) {
+        url = "/blog";
+      } else {
+        url = "http://www.bloomberg.com/news/2014-11-18/united-orbitz-sue-travel-site-over-hidden-city-ticketing-1-.html";
+      }
+      window.location = url;
+    });
+  }, 500);
+
   $('.let-me-proceed').click(function(e){
     e.preventDefault();
     var text = "Sorry for the inconvenience..."
